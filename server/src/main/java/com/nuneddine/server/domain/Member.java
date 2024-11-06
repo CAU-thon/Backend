c2a58d8e85cad255ee624af0c85d5d7dfcb160b7
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2024-11-06 23:55:49 +0900

    FEAT: Update Member class

--- server/src/main/java/com/nuneddine/server/domain/Member.java
+++ server/src/main/java/com/nuneddine/server/domain/Member.java
@@ -1,7 +1,28 @@
 package com.nuneddine.server.domain;
 
 
-@Entity
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import static jakarta.persistence.GenerationType.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "memberId")
+    private Long id;
+
+    @Column(unique = true)
+    private Long kakaoId;
+    private String username;
+    private String nickname;
+    private int build;
+    private int chance;
+    private int point;
 }
--- server/src/main/java/com/nuneddine/server/domain/MemberSnowman.java
+++ server/src/main/java/com/nuneddine/server/domain/MemberSnowman.java
@@ -1,4 +1,26 @@
 package com.nuneddine.server.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberSnowman {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "memberSnowmanId")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "memberId")
+    private Member member;
+
+
 }
