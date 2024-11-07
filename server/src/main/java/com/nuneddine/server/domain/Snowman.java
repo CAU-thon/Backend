15348e6aa753e6416337cb47e77c74a119d5b24d
kmh0601 <pongkmh@naver.com>
2024-11-07 20:17:22 +0900

    FEAT:snowman Id추가

--- server/src/main/java/com/nuneddine/server/domain/Snowman.java
+++ server/src/main/java/com/nuneddine/server/domain/Snowman.java
@@ -1,4 +1,21 @@
 package com.nuneddine.server.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Snowman {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "snowmanId")
+    private Long id;
 }
