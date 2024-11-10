a2875c4ac220c5b7b2144d46c080f1d1a338df8a
only4wxx <orchidbyw1@gmail.com>
2024-11-10 21:28:40 +0900

    Feat: Snowman 관련 Repository 추가

--- server/src/main/java/com/nuneddine/server/repository/SnowmanRepository.java
+++ server/src/main/java/com/nuneddine/server/repository/SnowmanRepository.java
@@ -1,7 +1,14 @@
 package com.nuneddine.server.repository;
 
+import com.nuneddine.server.domain.Member;
 import com.nuneddine.server.domain.Snowman;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface SnowmanRepository extends JpaRepository<Snowman, Long> {
+    Snowman findById(int id);
+    List<Snowman> findByMap(int map);
+    List<Snowman> findByMember(Member member);
 }
