99baaca97eeb6e840cb03139ed94394dca8bdda1
kmh0601 <pongkmh@naver.com>
2024-11-10 16:30:11 +0900

    FEAT:아이템카테고리 Enum 추가

--- server/src/main/java/com/nuneddine/server/domain/ItemCategory.java
+++ server/src/main/java/com/nuneddine/server/domain/ItemCategory.java
@@ -1,4 +1,23 @@
 package com.nuneddine.server.domain;
 
 public enum ItemCategory {
+    SHAPE("Shape"),
+    COLOR("Color"),
+    EYE("Eye"),
+    NOSE("Nose"),
+    MOUTH("Mouth"),
+    HAT("Hat"),
+    MUFFLER("Muffler"),
+    COAT("Coat"),
+    SHIRT("Shirt"); // custom은 여기에
+
+    private final String label;
+
+    ItemCategory(String label) {
+        this.label = label;
+    }
+
+    public String label() {
+        return label;
+    }
 }
