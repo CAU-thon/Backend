b36795c9ad9cb3ae77b547a70101bf47b4f0d75d
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2024-11-10 16:53:44 +0900

    FEAT: Member, Snowman 클래스 BaseTimeEntity 상속 받음

--- server/src/main/java/com/nuneddine/server/repository/MemberRepository.java
+++ server/src/main/java/com/nuneddine/server/repository/MemberRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface MemberJpaRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
     public Optional<Member> findByKakaoId(Long kakaoId);
 }
