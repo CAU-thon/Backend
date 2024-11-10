6f46a986890ab0bc58f14e1eafe2f045cb3925f0
only4wxx <orchidbyw1@gmail.com>
2024-11-10 21:52:47 +0900

    Feat: map에 있는 snowman 리스트업 - 눈사람의 id, 이미지, 위치를 반환

---
+++ server/src/main/java/com/nuneddine/server/service/SnowmanService.java
@@ -0,0 +1,27 @@
+package com.nuneddine.server.service;
+
+import com.nuneddine.server.domain.Snowman;
+import com.nuneddine.server.dto.response.SnowmanResponseDto;
+import com.nuneddine.server.repository.SnowmanRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SnowmanService {
+    @Autowired
+    SnowmanRepository snowmanRepository;
+
+    @Transactional
+    private List<SnowmanResponseDto> getSnowmansByMap(int mapNumber) {
+        List<Snowman> snowmans = snowmanRepository.findByMap(mapNumber);
+
+        // 눈사람의 id, 이미지, 위치를 반환
+        return snowmans.stream()
+                .map(snowman -> new SnowmanResponseDto(snowman.getId(), snowman.getImage(), snowman.getPosX(), snowman.getPosY()))
+                .collect(Collectors.toList());
+    }
+}
