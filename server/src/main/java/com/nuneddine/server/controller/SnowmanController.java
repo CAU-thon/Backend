33670018b95a0d7c895f1e49300e25b0556e5fd6
only4wxx <orchidbyw1@gmail.com>
2024-11-10 22:15:56 +0900

    Feat: 맵 눈사람 리스트업 기능 uri 매핑

---
+++ server/src/main/java/com/nuneddine/server/controller/SnowmanController.java
@@ -0,0 +1,31 @@
+package com.nuneddine.server.controller;
+
+import com.nuneddine.server.dto.response.SnowmanResponseDto;
+import com.nuneddine.server.service.SnowmanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1") // URL 주소 매핑
+public class SnowmanController {
+    @Autowired
+    SnowmanService snowmanService;
+
+    // 맵 눈사람 리스트업
+    @GetMapping("/{mapNumber}")
+    public ResponseEntity<List<SnowmanResponseDto>> getSnowmansByMapNumber(@PathVariable(value = "mapNumber") int mapNumber) {
+        List<SnowmanResponseDto> snowmans = snowmanService.findSnowmansByMap(mapNumber);
+        if (snowmans.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        else {
+            return ResponseEntity.ok(snowmans);
+        }
+    }
+}
