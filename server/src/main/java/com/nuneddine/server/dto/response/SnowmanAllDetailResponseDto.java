15d895995923df0438b8e9f6881e9c94a05f8ff9
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2024-11-17 01:00:07 +0900

    REMOVE: Unusing field in SnowmanAllDetailResponseDto

--- server/src/main/java/com/nuneddine/server/dto/response/SnowmanAllDetailResponseDto.java
+++ server/src/main/java/com/nuneddine/server/dto/response/SnowmanAllDetailResponseDto.java
@@ -10,16 +10,11 @@ import java.time.LocalDateTime;
 public class SnowmanAllDetailResponseDto {
     private Long id;
     private String name;
-//    private String color;
-//    private SnowmanShape snowmanShape;
     private String image; // 눈사람 이미지
     private int mapNumber; // 눈사람이 있는 맵
     private double posX; // 눈사람의 위치 x좌표
     private double posY; // 눈사람의 위치 y좌표
     private Long userId; // 해당 눈사람을 만든 사용자 id
-//    private String username; // 해당 눈사람을 만든 사용자 이름
-
-//    private List<SnowmanItemResponseDto> snowmanItems;
 
     private String quiz; // 눈사람 문제 내용
     private Long answerId; // 눈사람 문제 답
