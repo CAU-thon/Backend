256598b7ff4cee62889220e3ff5354eabfb0711c
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2024-11-16 21:29:25 +0900

    FEAT: Modify file upload logic form-data -> base64

--- server/src/main/java/com/nuneddine/server/service/FileUploadService.java
+++ server/src/main/java/com/nuneddine/server/service/FileUploadService.java
@@ -9,9 +9,10 @@ import com.nuneddine.server.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.Base64;
 import java.util.UUID;
 
 @Service
@@ -23,17 +24,24 @@ public class FileUploadService {
     @Value("${s3.bucket-name}")
     private String bucketName;
 
-    public String uploadImage(MultipartFile file) {
-        String fileName = "images/" + UUID.randomUUID() + "_" + file.getOriginalFilename();
+    // DTO에서 받은 base64 인코딩 된 값을 매개변수로 입력
+    public String uploadImage(String base64Image) {
+        byte[] decodedBytes;
+        try {
+            decodedBytes = Base64.getDecoder().decode(base64Image.split(",")[1]); // "data:image/png;base64," 제거
+        } catch (IllegalArgumentException e) {
+            throw new CustomException(ErrorCode.INVALID_BASE64_DATA);
+        }
+        String fileName = "images/" + UUID.randomUUID() + ".png";
 
         // S3에 저장할 메타데이터 설정
         ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentLength(file.getSize());
-        metadata.setContentType(file.getContentType());
+        metadata.setContentLength(decodedBytes.length);
+        metadata.setContentType("image/png");
 
         // 파일 업로드
-        try {
-            amazonS3.putObject(bucketName, fileName, file.getInputStream(), metadata);
+        try (ByteArrayInputStream inputStream = new ByteArrayInputStream(decodedBytes)) {
+            amazonS3.putObject(bucketName, fileName, inputStream, metadata);
         } catch (AmazonServiceException e) {
             throw new CustomException(ErrorCode.AWS_S3_ACCESS_DENIED);
         } catch (AmazonClientException e) {
